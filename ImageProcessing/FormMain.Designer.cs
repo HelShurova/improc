903626d98c965448bf44572cff2364eb65ed3dd4
Daria Sokolova <dashus94@gmail.com>
2015-09-19 12:28:26 +0300

    Added scrollbar.

--- ImageProcessing/FormMain.Designer.cs
+++ ImageProcessing/FormMain.Designer.cs
@@ -32,6 +32,7 @@
             this.buttonLoadImage = new System.Windows.Forms.Button();
             this.openImageDialog = new System.Windows.Forms.OpenFileDialog();
             this.panelImage = new System.Windows.Forms.Panel();
+            this.hScrollBarFilterIntensity = new System.Windows.Forms.HScrollBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
             this.panelImage.SuspendLayout();
             this.SuspendLayout();
@@ -49,7 +50,7 @@
             // 
             this.buttonLoadImage.Location = new System.Drawing.Point(627, 347);
             this.buttonLoadImage.Name = "buttonLoadImage";
-            this.buttonLoadImage.Size = new System.Drawing.Size(96, 43);
+            this.buttonLoadImage.Size = new System.Drawing.Size(180, 43);
             this.buttonLoadImage.TabIndex = 1;
             this.buttonLoadImage.Text = "Загрузить изображение";
             this.buttonLoadImage.UseVisualStyleBackColor = true;
@@ -69,12 +70,21 @@
             this.panelImage.Size = new System.Drawing.Size(609, 378);
             this.panelImage.TabIndex = 2;
             // 
+            // hScrollBarFilterIntensity
+            // 
+            this.hScrollBarFilterIntensity.Location = new System.Drawing.Point(627, 310);
+            this.hScrollBarFilterIntensity.Name = "hScrollBarFilterIntensity";
+            this.hScrollBarFilterIntensity.Size = new System.Drawing.Size(180, 20);
+            this.hScrollBarFilterIntensity.TabIndex = 3;
+            this.hScrollBarFilterIntensity.Value = 46;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(735, 402);
+            this.ClientSize = new System.Drawing.Size(819, 402);
+            this.Controls.Add(this.hScrollBarFilterIntensity);
             this.Controls.Add(this.panelImage);
             this.Controls.Add(this.buttonLoadImage);
             this.Name = "FormMain";
@@ -91,6 +101,7 @@
         private System.Windows.Forms.Button buttonLoadImage;
         private System.Windows.Forms.OpenFileDialog openImageDialog;
         private System.Windows.Forms.Panel panelImage;
+        private System.Windows.Forms.HScrollBar hScrollBarFilterIntensity;
     }
 }
 
